bba4c02f77b767d87af1ad92f6bf884210f3f39e
Akash Kumar <akp31378@gmail.com>
2022-12-29 16:28:24 +0530

    AKASH KUMAR

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const PORT = process.env.PORT || 3889;
+app.set("view engine", "hbs");
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+app.listen(PORT, () => {
+    console.log("Server is started at port " + PORT);
+})
